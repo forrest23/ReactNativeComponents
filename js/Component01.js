38c04bf517ce9adda6150823f4dceace3b89f7e3
forrest23 <forrest23@126.com>
2017-12-05 10:53:54 +0800

    截图测试

--- js/Component01.js
+++ js/Component01.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
-import ViewShot from "react-native-view-shot";
+import { StyleSheet, Text, View, ScrollView, Button, Image } from 'react-native';
+import captureRef from "react-native-view-shot";
 
 export default class Component01 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageUrl: 'https://pic.yupoo.com/forrest071/FXTxp6DG/medium.jpg',
+            value: {
+                format: "png",
+                quality: 0.9,
+                result: "tmpfile",
+                snapshotContentContainer: false
+              }
+        };
+    }
+
     doCapture = () => {
-        this.refs.viewShot.capture().then(uri => {
-            alert(uri);
-        });
+        console.log(11111)
+         captureRef(this.refs.viewShot, this.state.value)
+         console.log(this.state.value.result);
+    }
+
+    onCapture = uri => {
+        console.log("do something with ", uri);
+        this.setState({ imageUrl: uri });
     }
 
     render() {
         return (
-            <ScrollView>
-                <ViewShot ref="viewShot" >
-                    <Text>...The Scroll View Content Goes Here...</Text>
-                    <Button title="截图" onPress={() => this.doCapture}
-                    />
-                </ViewShot>
+            <ScrollView ref="viewShot" >
+                <Text>...The Scroll View Content Goes Here...</Text>
+                <Button title="截图" onPress={() => this.doCapture()} />
+                <Image
+                    style={styles.logo}
+                    source={{ uri: this.state.imageUrl }}
+                />
             </ScrollView>
         );
     }
@@ -29,4 +48,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    logo: {
+        width: 300,
+        height: 297
+    }
 });
